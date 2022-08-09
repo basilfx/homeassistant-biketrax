39458142326c5a7c4fd6168889fef29a6fd46479
vlebourl <vlebourl@gmail.com>
2022-08-09 10:00:33 +0200

    fix alarm_control_panel needing both home and away

--- custom_components/biketrax/alarm_control_panel.py
+++ custom_components/biketrax/alarm_control_panel.py
@@ -1,26 +1,26 @@
 """Support for Overkiz Alarms."""
 from __future__ import annotations
 
-import logging
-
 from aiobiketrax import Device
 from homeassistant.components.alarm_control_panel import DOMAIN as ALARM_DOMAIN
 from homeassistant.components.alarm_control_panel import (
     AlarmControlPanelEntity,
 )
 from homeassistant.components.alarm_control_panel.const import (
-    SUPPORT_ALARM_ARM_AWAY,
+    AlarmControlPanelEntityFeature,
 )
 from homeassistant.config_entries import ConfigEntry
-from homeassistant.const import STATE_ALARM_ARMED_AWAY, STATE_ALARM_DISARMED
+from homeassistant.const import (
+    STATE_ALARM_ARMED_AWAY,
+    STATE_ALARM_ARMED_HOME,
+    STATE_ALARM_DISARMED,
+)
 from homeassistant.core import HomeAssistant
 from homeassistant.helpers.entity_platform import AddEntitiesCallback
 
 from . import BikeTraxBaseEntity
 from .const import DATA_DEVICE, DOMAIN
 from .coordinator import BikeTraxDataUpdateCoordinator
-
-_LOGGER = logging.getLogger(__name__)
 
 
 async def async_setup_entry(
@@ -44,7 +44,10 @@ async def async_setup_entry(
 class BikeTraxAlarmController(BikeTraxBaseEntity, AlarmControlPanelEntity):
     """Representation of a BikeTrax Alarm."""
 
-    _attr_supported_features = SUPPORT_ALARM_ARM_AWAY
+    _attr_supported_features = (
+        AlarmControlPanelEntityFeature.ARM_HOME
+        | AlarmControlPanelEntityFeature.ARM_AWAY
+    )
 
     def __init__(
         self,
@@ -57,20 +60,30 @@ class BikeTraxAlarmController(BikeTraxBaseEntity, AlarmControlPanelEntity):
         self.entity_id = f"{ALARM_DOMAIN}.{DOMAIN}_alarm_{device.id}"
         self._attr_name = f"{device.name} Alarm"
         self._attr_unique_id = f"{device.id}-alarm"
+        self._is_home = False
 
     @property
     def state(self):
         """Return the state of the device."""
         return (
-            STATE_ALARM_ARMED_AWAY
+            STATE_ALARM_ARMED_HOME
+            if self.device.is_guarded and self._is_home
+            else STATE_ALARM_ARMED_AWAY
             if self.device.is_guarded
             else STATE_ALARM_DISARMED
         )
 
     async def async_alarm_disarm(self, code=None):
         """Send disarm command."""
+        self._is_home = False
         await self.device.set_guarded(False)
+
+    async def async_alarm_arm_home(self, code=None):
+        """Send arm away command."""
+        self._is_home = True
+        await self.device.set_guarded(True)
 
     async def async_alarm_arm_away(self, code=None):
         """Send arm away command."""
+        self._is_home = False
         await self.device.set_guarded(True)
